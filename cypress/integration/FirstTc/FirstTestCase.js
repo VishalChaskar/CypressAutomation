76f8da9c02538c0aa49cc789126f2260b5341b58
Vishal <chaskar.vishal7535@gmail.com>
2021-10-20 00:48:34 +0530

    In FirstTestCase add 2nd fucntion for amazon

--- cypress/integration/FirstTc/FirstTestCase.js
+++ cypress/integration/FirstTc/FirstTestCase.js
@@ -3,6 +3,9 @@ describe('My First TestCase',function()
 it('navigate to google',function(){
     cy.visit("www.flipkart.com")
 })
+it('navigate to amazon',()=>{
+cy.visit("www.amazon.com")
+})
 
 
 
