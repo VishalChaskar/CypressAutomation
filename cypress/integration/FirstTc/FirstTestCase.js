f0f9c9e1d8b865fccae58bc96014bf0df47fbdab
Vishal <chaskar.vishal7535@gmail.com>
2021-10-20 00:58:18 +0530

    apple.com fisttestcase_3function

--- cypress/integration/FirstTc/FirstTestCase.js
+++ cypress/integration/FirstTc/FirstTestCase.js
@@ -4,12 +4,11 @@ it('navigate to google',function(){
     cy.visit("www.flipkart.com")
 })
 it('navigate to amazon',()=>{
-cy.visit("www.amazon.com")
+cy.visit('www.amazon.com')
 })
 
-
-
-
-
+it('navigate to apple.com',function(){
+cy.visit('www.apple.com')
+})
 
 })
