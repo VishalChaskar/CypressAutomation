1092ddf736bfbcfcd35f42c4c3932777f74beb6a
Vishal <chaskar.vishal7535@gmail.com>
2021-10-26 02:25:52 +0530

    VeggieShopTC

---
+++ cypress/integration/RahulShettyAutomation/VeggieShop.js
@@ -0,0 +1,13 @@
+describe('My First TestCase',function()
+{
+it('navigate to veggieshop',function(){
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product').should('have.length',5)
+    cy.get('.products').find('.product').should('have.length',4)
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+    cy.wait(2000)
+
+})
+})
